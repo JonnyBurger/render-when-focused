875821caf0b2a45388d85cabad7f316dd0ab27f0
Jonny Burger <jonathanburger11@gmail.com>
2018-11-10 15:06:26 +0100

    fix a syntax error

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const React = require('react');
 const {withNavigationFocus} = require('react-navigation');
 const hoistNonReactStatics = require('hoist-non-react-statics');
 
-module.exports = functin (Comp) {
-    class Hoc extends React.Component {
-        shouldComponentUpdate(nextProps) {
-            return nextProps.isFocused;
-        }
-        render() {
-            return React.createElement(Comp, this.props);
-        }
-    }
-    return hoistNonReactStatics(withNavigationFocus(Hoc), Comp);
-}
+module.exports = function(Comp) {
+	class Hoc extends React.Component {
+		shouldComponentUpdate(nextProps) {
+			return nextProps.isFocused;
+		}
+		render() {
+			return React.createElement(Comp, this.props);
+		}
+	}
+	return hoistNonReactStatics(withNavigationFocus(Hoc), Comp);
+};
